6aa83a030eeb83b4162b417209ea2165c3e075f9
Robloxer9000 <167287547+Robloxer9000@users.noreply.github.com>
2024-05-24 20:42:11 +0800

    Update hardmodev2.5.py

--- launchpad/hardmodev2.5.py
+++ launchpad/hardmodev2.5.py
@@ -7,66 +7,67 @@ def pixel(buttonid, colour): #function to light up 1 pixel
     outport.send(msg)
 
 def button1(northeast):
-    pixel(65, northeast)
-button1(10)
+    pixel(75, northeast)
+button1(63)
 
 def button2(east):
-    pixel(55, east)
+    pixel(65, east)
 button2(4)
 
 def button3(southeast):
-    pixel(45, southeast)
+    pixel(55, southeast)
 button3(30)
 
 def button4(southwest):
-    pixel(43, southwest)
-button4(30)
+    pixel(53, southwest)
+button4(10)
 
 def button5(west):
-    pixel(53, west)
-button5(4)
+    pixel(63, west)
+button5(53)
 
 def button6(northwest):
-    pixel(63, northwest)
-button6(10)
+    pixel(73, northwest)
+button6(40)
 
 
 
 
+def NW(speaker):
+	pixel(81, speaker)
+NW(40)
 
-def group1(speakers1):
-	buttonid = [71, 78]
-	for i in buttonid:
-		pixel(i, speakers1)
-group1(10)
+def W(speaker):
+	pixel(61, speaker)
+W(53)
 
-def group2(speakers2):
-	buttonid = [51, 58]
-	for i in buttonid:
-		pixel(i, speakers2)
-group2(4)
+def SW(speaker):
+	pixel(41, speaker)
+SW(10)
 
-def group3(speakers3):
-	buttonid = [31, 38]
-	for i in buttonid:
-		pixel(i, speakers3)
-group3(30)
+def NE(speaker):
+	pixel(87, speaker)
+NE(63)
+
+def E(speaker):
+	pixel(67, speaker)
+E(4)
+
+def SE(speaker):
+	pixel(47, speaker)
+SE(30)
+
+
+
+def middle(white):
+	pixel(64, white)
+middle(3)
 
 def box(white):
-	buttonid = [72, 73, 74, 75, 76, 66, 56, 46, 36, 35, 34, 33, 32, 42, 52, 62]
+	buttonid = [82, 83, 84, 85, 86, 76, 66, 56, 46, 45, 44, 43, 42, 52, 62, 72]
 	for i in buttonid:
 		pixel(i, white)
 box(3)
-
-def arrow(white):	
-	buttonid = [13,14,15,24]
-	for i in buttonid:
-		pixel(i, white)
-arrow(3)
-
-def middle(white):
-	pixel(54)
-middle(3)
             
 def pressNE():
     
@@ -97,16 +98,16 @@ def pressNW():
     
 
 for msg in inport:
-    if msg.type == 'note_on' and msg.note == 65  and msg.velocity != 0:
+    if msg.type == 'note_on' and msg.note == 75  and msg.velocity != 0:
         pressNE()
-    elif msg.type == 'note_on' and msg.note == 55 and msg.velocity != 0:
+    elif msg.type == 'note_on' and msg.note == 65 and msg.velocity != 0:
         pressE()
-    elif msg.type == 'note_on' and msg.note == 45 and msg.velocity != 0:
+    elif msg.type == 'note_on' and msg.note == 55 and msg.velocity != 0:
         pressSE()
-    elif msg.type == 'note_on' and msg.note == 43 and msg.velocity != 0:
-        pressSW()
     elif msg.type == 'note_on' and msg.note == 53 and msg.velocity != 0:
-        pressW()
+        pressSW()
     elif msg.type == 'note_on' and msg.note == 63 and msg.velocity != 0:
+        pressW()
+    elif msg.type == 'note_on' and msg.note == 73 and msg.velocity != 0:
         pressNW()
 	
