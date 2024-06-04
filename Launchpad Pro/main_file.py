6db2b32cb7e4c62e3d753a34e1f615c9ed16928f
Robloxer9000 <167287547+Robloxer9000@users.noreply.github.com>
2024-06-04 19:24:41 +0800

    Launchpad Final Draft

--- Launchpad Pro/main_file.py
+++ Launchpad Pro/main_file.py
@@ -1,190 +1,100 @@
 import mido
+import threading
+import subprocess
+import time
+
+# Set up Launchpad MIDI ports for input and output
 midipad = 'Launchpad Pro MK3:Launchpad Pro MK3 LPProMK3 MIDI 28:0'
-outport = mido.open_output(midipad) # output port for sending commands
-inport = mido.open_input(midipad) # input port for feedback from launchpad
+outport = mido.open_output(midipad)  # Output port for sending commands to Launchpad
+inport = mido.open_input(midipad)  # Input port for receiving feedback from Launchpad
 
-ogcolour = {
-    81: 40,  # Colours for NORTH WEST
-    73: 40,  
-    51: 53,  # Colours foR WEST
-    53: 53,  
-    33: 10,  # Colours for SOUTH WEST
-    21: 10,  
-    35: 30,  # Colours for SOUTH EAST
-    27: 30,  
-    55: 4,  # Colours for EAST
-    57: 4,  
-    75: 63,  # Colours for NORTH EAST
-    87: 63,  
-    
-}
-
-def pixel(buttonid, colour): #function to light up 1 pixel
+def pixel(buttonid, colour):
+    """Function to light up 1 pixel."""
+    # Output command to send color to a specific button
     msg = mido.Message('note_on', note=buttonid, velocity=colour)
     outport.send(msg)
 
-def initialize_buttons():
-    pixel(64, ogcolour[64])
-    for i in range(83, 86):
-        pixel(i, ogcolour[i])
-    button1(63)
-    button2(55)
-    button3(30)
-    button4(10)
-    button5(53)
-    button6(40)
-    middle(3)
-    NW(40)
-    W(53)
-    SW(10)
-    NE(63)
-    E(4)
-    SE(30)
-    box(3)
+# Global variables to keep track of the running subprocess and mode
+current_process = None
+current_mode = None
+start_button_active = True  # Flag to track if the start button is active
+mode_buttons_active = False  # Flag to track if mode buttons are active
 
-def button1(northeast):
-    pixel(75, northeast)
+def terminate_current_process():
+    """Function to terminate the current process if it exists."""
+    global current_process
+    if current_process:
+        print("Terminating current process...")
+        current_process.terminate()  # Terminate the running subprocess
+        current_process.wait()
+        current_process = None
+        print("Process terminated.")
 
-def button2(east):
-    pixel(55, east)
+def start_easy_mode():
+    """Function to start easy mode."""
+    global current_process, current_mode
+    if current_mode != 'easy':  # Only start if not already in easy mode
+        terminate_current_process()
+        print("Starting easy mode...")
+        current_process = subprocess.Popen(['python', 'easymode.py'])  # Start easy mode subprocess
+        current_mode = 'easy'
+        clear_board()
+        light_mode_buttons()  # Ensure mode buttons remain lit
 
-def button3(southeast):
-    pixel(35, southeast)
+def start_hard_mode():
+    """Function to start hard mode."""
+    global current_process, current_mode
+    if current_mode != 'hard':  # Only start if not already in hard mode
+        terminate_current_process()
+        print("Starting hard mode...")
+        current_process = subprocess.Popen(['python', 'hardmode2.py'])  # Start hard mode subprocess
+        current_mode = 'hard'
+        clear_board()
+        light_mode_buttons()  # Ensure mode buttons remain lit
 
-def button4(southwest):
-    pixel(33, southwest)
+def clear_board():
+    """Function to clear the LED board."""
+    # Output command to clear lights of the whole board
+    for i in range(0, 128):
+        pixel(i, 0)
 
-def button5(west):
-    pixel(53, west)
+def light_mode_buttons():
+    """Function to light up the mode buttons."""
+    # Output commands to light up specific mode buttons
+    pixel(17, 64)  
+    pixel(18, 96)  
 
-def button6(northwest):
-    pixel(73, northwest)
+def handle_midi_input():
+    """Function to handle incoming MIDI messages."""
+    global start_button_active, mode_buttons_active
+    for msg in inport:  # Input feedback from Launchpad
+        if msg.type == 'note_on' and msg.velocity > 0:
+            if msg.note == 54 and start_button_active:
+                start_button_active = False
+                mode_buttons_active = True
+                pixel(54, 0)  # Turn off the LED for button 54
+                start_easy_mode()  # Default to easy mode
+                light_mode_buttons()  # Light up mode buttons
+            elif msg.note == 18 and mode_buttons_active:
+                start_hard_mode()
+            elif msg.note == 17 and mode_buttons_active:
+                start_easy_mode()
 
+def start_midi_listener():
+    """Function to run the MIDI input handler in a separate thread."""
+    listener_thread = threading.Thread(target=handle_midi_input)
+    listener_thread.daemon = True
+    listener_thread.start()
 
+if __name__ == "__main__":
+    # Output command to light up button 54 as white to indicate it's the start button
+    pixel(54, 127)  # 127 is usually the highest value for full brightness
 
+    # Start listening for MIDI input
+    start_midi_listener()  # Start MIDI listener in a separate thread
 
-def NW(speaker):
-	pixel(81, speaker)
+    # Keep the main thread alive
+    while True:
+        time.sleep(1)  # Prevent high CPU usage
 
-def W(speaker):
-	pixel(51, speaker)
-
-def SW(speaker):
-	pixel(21, speaker)
-
-def NE(speaker):
-	pixel(87, speaker)
-
-def E(speaker):
-	pixel(57, speaker)
-
-def SE(speaker):
-	pixel(27, speaker)
-
-
-
-def middle(white):
-	pixel(54, white)
-
-def box(white):
-	buttonid = [82, 83, 84, 85, 86, 76, 66, 56, 46, 36, 26, 25, 24, 23, 22, 32, 42, 52, 62, 72]
-	for i in buttonid:
-		pixel(i, white)
-            
-def pressNE():
-    
-    print("NORTH EAST" )
-    pixel(75, 3)  # Change to white
-    pixel(87, 3)  # Change to white
-
-def releaseNE():
-    pixel(75, ogcolour[75])  # Change back to original color
-    pixel(87, ogcolour[87])  # Change back to original color
-
-def pressE():
-    
-    print("EAST" )
-    pixel(55, 3)  # Change to white
-    pixel(57, 3)  # Change to white
-
-def releaseE():
-    pixel(55, ogcolour[55])  # Change back to original color
-    pixel(57, ogcolour[57])  # Change back to original color
-
-def pressSE():
-    
-    print("SOUTH EAST" )
-    pixel(35, 3)  # Change to white
-    pixel(27, 3)  # Change to white
-
-def releaseSE():
-    pixel(35, ogcolour[35])  # Change back to original color
-    pixel(27, ogcolour[27])  # Change back to original color
-
-def pressSW():
-    
-    print("SOUTH WEST" )
-    pixel(33, 3)  # Change to white
-    pixel(21, 3)  # Change to white
-
-def releaseSW():
-    pixel(33, ogcolour[33])  # Change back to original color
-    pixel(21, ogcolour[21])  # Change back to original color
-
-def pressW():
-    
-    print("WEST" )
-    pixel(53, 3)  # Change to white
-    pixel(51, 3)  # Change to white
-
-def releaseW():
-    pixel(73, ogcolour[53])  # Change back to original color
-    pixel(81, ogcolour[51])  # Change back to original color
-
-def pressNW():
-    
-    print("NORTH WEST" )
-    pixel(73, 3)  # Change to white
-    pixel(81, 3)  # Change to white
-
-def releaseNW():
-    pixel(73, ogcolour[73])  # Change back to original color
-    pixel(81, ogcolour[81])  # Change back to original color
-
-
-
-    
-# Press for feedback function
-for msg in inport:
-    if msg.type == 'note_on' or msg.type == 'note_off':
-        if msg.note == 75:
-            if msg.velocity != 0:  # When North East pressed
-                pressNE()
-            else:   # When North East is released
-                releaseNE()
-        elif msg.note == 55:
-            if msg.velocity != 0:  
-                pressE()
-            else: 
-                releaseE()  
-        elif msg.note == 35:
-            if msg.velocity != 0:  
-                pressSE()
-            else: 
-                releaseSE() 
-        elif msg.note == 33:
-            if msg.velocity != 0:  
-                pressSW()
-            else: 
-                releaseSW() 
-        elif msg.note == 53:
-            if msg.velocity != 0:  
-                pressW()
-            else: 
-                releaseW()    
-        elif msg.note == 73:
-            if msg.velocity != 0: 
-                pressNW()
-            else: 
-                releaseNW()  
                                                                                                                           
